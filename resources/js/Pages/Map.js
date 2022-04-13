a9084a90ae3e4cb992041c13ed5a69c1b3ac1c01
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2022-04-13 11:15:33 +0700

    first commit

---
+++ resources/js/Pages/Map.js
@@ -0,0 +1,31 @@
+import Authenticated from "@/Layouts/Authenticated";
+import 'leaflet/dist/leaflet.css'
+import React from "react";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Map(props) {
+    return (
+        <Authenticated
+            auth={props.auth}
+            errors={props.errors}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Map
+                </h2>
+            }
+        >
+            <MapContainer center={[51.505, -0.09]} zoom={13}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </Authenticated>
+    );
+}
